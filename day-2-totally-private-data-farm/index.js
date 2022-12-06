09172afe83c088c49abcdbf573803b4d25e419c2
Jessica Nash <jessicanashacc@gmail.com>
2022-12-06 20:17:06 +0900

    finished day 2

---
+++ day-2-totally-private-data-farm/index.js
@@ -0,0 +1,37 @@
+import userData from "./data.js";
+
+/* Totally Private Data Farm 
+
+Good news, renown advertising firm Evil Corp. wants to purchase our 
+private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+    fullName: "Levent Busser", 
+    birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+
+*/
+function transformData(data){
+    return data.map(object => {
+        
+        return {
+            fullname: `${object.name.first} ${object.name.last}`,
+            birthday: new Date(object.dob.date).toDateString()
+        };
+    })
+}
+
+console.log(transformData(userData));
