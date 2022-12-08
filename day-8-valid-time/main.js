554dcafbca27aa3ecee7d2c2eb9c0c35991c0ebb
Jessica Nash <jessicanashacc@gmail.com>
2022-12-08 18:39:04 +0900

    day 8 finished :D

---
+++ day-8-valid-time/main.js
@@ -0,0 +1,56 @@
+function validTime(str) {
+    //  write code here.
+    const hour = Number.parseInt(str.split(":")[0]);
+    const min = Number.parseInt(str.split(":")[1]);
+    return (hour > 24 || hour < 1 || min > 60 || min < 1) ? false : true;
+}
+
+console.log(validTime("08:25"))
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
