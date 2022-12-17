886b3eb2dcccf866456361c15217fb28c37530f7
Jessica Nash <jessicanashacc@gmail.com>
2022-12-17 22:17:36 +0900

    day 16 finished

---
+++ day-16-insert-dashes/main.js
@@ -0,0 +1,25 @@
+function insertDashes(arr) {
+    // write code here
+    return arr.split(" ").map(word=>{return word.split("").join("-")}).join(" ");
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('insertDashes()', () => {
+    it('insert dashes in between chars', () => {
+        // arrange
+        const value = "aba caba";
+        
+        // act
+        const result = insertDashes(value);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toBe("a-b-a c-a-b-a");
+    });
+});
